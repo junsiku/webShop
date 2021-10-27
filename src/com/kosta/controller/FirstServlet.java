915116d21ee35b1e0a818e4971d3de9b6f778c97
junsiku <junsik7511@gmail.com>
2021-10-27 09:35:40 +0900

    10-27
    Signed-off-by: junsiku <junsik7511@gmail.com>

--- src/com/kosta/controller/FirstServlet.java
+++ src/com/kosta/controller/FirstServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
+///1111111
 @WebServlet({ "/FirstServlet", "/first", "/monday/one" })
 public class FirstServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
